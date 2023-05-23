7a6a7c7339aef1adadd76fce0e72d7961189fbaf
TESTAPIS21 <aguimashoes362@gmail.com>
2023-05-23 20:14:58 +0000

    a

---
+++ f_gastos/f_gastos/doctype/tax_withholding_categorys/tax_withholding_categorys.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, sistemas and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Tax Withholding Categorys', {
+	// refresh: function(frm) {
+
+	// }
+});
