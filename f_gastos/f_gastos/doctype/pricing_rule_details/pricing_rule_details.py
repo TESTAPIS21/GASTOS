f89b9d940ac3a568f68bcf764cb201a49bcc46f1
TESTAPIS21 <aguimashoes362@gmail.com>
2023-05-23 20:22:34 +0000

    asd

--- f_gastos/f_gastos/doctype/pricing_rule_details/pricing_rule_details.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, sistemas and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class PricingRuleDetails(Document):
-	pass
